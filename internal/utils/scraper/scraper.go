b72eb63ab7e1aacad7e5631c9cc889badcd515bf
Moeein Aali <moeeeinaali@gmail.com>
2024-11-08 22:47:35 +0330

    feat: add scraper

--- internal/utils/scraper/scraper.go
+++ internal/utils/scraper/scraper.go
@@ -1,8 +1,107 @@
 package scraper
 
-import "creepy/internal/property"
+import (
+	"creepy/internal/property"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/playwright-community/playwright-go"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
-func ScrapePropertyData(url string, linkType string) (*property.Property, error) {
+type LinkType string
 
-	return nil, nil
+const (
+	LinkType_Divar_Buy  LinkType = "Divar_Buy"
+	LinkType_Divar_Rent LinkType = "Divar_Rent"
+)
+
+func ScrapePropertyData(url string, linkType LinkType) (*property.Property, error) {
+	parts := strings.Split(string(linkType), "-")
+	platform := parts[0]
+	propertyType := parts[1]
+	selectors := Selectors[platform][propertyType]
+
+	pw, err := playwright.Run()
+	defer pw.Stop()
+	if err != nil {
+		return nil, fmt.Errorf("خطا در اجرای Playwright: %v", err)
+	}
+
+	browser, err := pw.Firefox.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(true),
+	})
+	defer browser.Close()
+	if err != nil {
+		return nil, fmt.Errorf("خطا در اجرای مرورگر: %v", err)
+	}
+	page, err := browser.NewPage()
+	if err != nil {
+		return nil, fmt.Errorf("خطا در ایجاد صفحه: %v", err)
+	}
+	if _, err := page.Goto(url); err != nil {
+		return nil, fmt.Errorf("خطا در باز کردن صفحه: %v", err)
+	}
+
+	resultProperty := property.Property{
+		ID:           uuid.UUID{},
+		Title:        "",
+		Description:  "",
+		BuyPrice:     0,
+		RentPrice:    0,
+		RentPriceMin: 0,
+		RentPriceMax: 0,
+		RahnPriceMin: 0,
+		RahnPriceMax: 0,
+		Area:         0,
+		Rooms:        0,
+		DealingType:  "",
+		Type:         "",
+		City:         "",
+		District:     "",
+		Address:      "",
+		BuildYear:    0,
+		Floor:        0,
+		HasElevator:  false,
+		HasStorage:   false,
+		Latitude:     0,
+		Longitude:    0,
+		Source:       "",
+		URL:          "",
+		Images:       nil,
+	}
+
+	for field, selector := range selectors {
+		content, err := page.TextContent(selector)
+		if err == nil {
+			switch field {
+			case "Title":
+				resultProperty.Title = content
+			case "Description":
+				resultProperty.Description = content
+			case "BuyPrice":
+				num, err := strconv.ParseUint(regexp.MustCompile(`[٬ تومان]`).ReplaceAllString(content, ""), 10, 64)
+				if err != nil {
+					resultProperty.BuyPrice = num
+				}
+			case "Area":
+				num, err := strconv.ParseUint(content, 10, 64)
+				if err != nil {
+					resultProperty.Area = num
+				}
+			case "Rooms":
+				num, err := strconv.ParseUint(content, 10, 0)
+				if err != nil {
+					resultProperty.Rooms = uint(num)
+				}
+			case "City":
+				resultProperty.City = content
+
+			}
+		}
+
+	}
+	fmt.Println(resultProperty)
+	return &resultProperty, nil
 }
--- internal/utils/scraper/selectors.go
+++ internal/utils/scraper/selectors.go
@@ -1,12 +1,14 @@
 package scraper
 
-var SELECTOR_DIVAR_BUY = map[string]string{
+type selector map[string]string
+
+var selectorDivarBuy = selector{
 	"Title":       "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.kt-page-title > div > h1",
 	"Description": "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section.post-page__section--padded > div > div.kt-base-row.kt-base-row--large.kt-description-row > div > p",
 	"BuyPrice":    "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > div:nth-child(3) > div.kt-base-row__end.kt-unexpandable-row__value-box > p",
 	"Area":        "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > table:nth-child(1) > tbody > tr > td:nth-child(1)",
 	"Rooms":       "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > table:nth-child(1) > tbody > tr > td:nth-child(3)",
-	"City":        "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.kt-page-title > div > div",
+	"City":        "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.kt-page-title > div > div"
 	"District":    "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.kt-page-title > div > div",
 	"BuildYear":   "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > table:nth-child(1) > tbody > tr > td:nth-child(2)",
 	"Floor":       "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > div:nth-child(7) > div.kt-base-row__end.kt-unexpandable-row__value-box > p",
@@ -14,4 +16,15 @@ var SELECTOR_DIVAR_BUY = map[string]string{
 	"HasStorage":  "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.post-page__section--padded > table:nth-child(10) > tbody > tr > td:nth-child(3)",
 	"Images":      "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-6.kt-offset-1 > section:nth-child(1) > div > div > div.kt-carousel__thumbnails > div"}
 
-var SELECTOR_DIVAR_RENT = map[string]string{}
+var selectorDivarRent = selector{}
+
+var Selectors = map[string]map[string]selector{
+	"Divar": {
+		"Buy":  selectorDivarBuy,
+		"Rent": selectorDivarRent,
+	},
+	"Sheypoor": {
+		"Buy":  nil,
+		"Rent": nil,
+	},
+}
