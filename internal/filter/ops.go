83172dc71bdb0a5410e01b5dacd2fb5b66df1aa1
momoein <momoein711@gmail.com>
2024-11-08 04:28:37 +0330

    feat (internal): initial ops

--- internal/filter/ops.go
+++ internal/filter/ops.go
@@ -2,24 +2,23 @@ package filter
 
 import "context"
 
-
 type Ops struct {
 	repo Repo
 }
 
-func NewOps(repo Repo) *Ops{
+func NewOps(repo Repo) *Ops {
 	return &Ops{repo}
 }
 
-func (o *Ops)CreateFiletr(ctx context.Context, f *FilterSet) error{
-	// validation check 
+func (o *Ops) CreateFilter(ctx context.Context, f *Filter) error {
+	// validation check
 	// minprice < max price
 	// price>0
 	//
 	// => convert persain to english numbers
 
 	err := o.repo.Insert(ctx, f)
-	if err!=nil{
+	if err != nil {
 		return ErrCreateFilter
 	}
 	return nil
