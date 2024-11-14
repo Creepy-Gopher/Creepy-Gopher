5270248078bd02b77966caa2f8e4b20df6cd210e
momoein <momoein711@gmail.com>
2024-11-15 01:18:09 +0330

    fix(models): update foreign key referencing users table

--- internal/models/bookmark.go
+++ internal/models/bookmark.go
@@ -4,8 +4,8 @@ import "github.com/google/uuid"
 
 type Bookmark struct {
 	Model
-	UserName   string
-	User       User     `gorm:"foreignKey:UserName"`
+	UserID     uuid.UUID
+	User       User     `gorm:"foreignKey:UserID"`
 	PropertyID uuid.UUID
 	Property   Property `gorm:"foreignKey:PropertyID"`
 	SharedWithMe bool
